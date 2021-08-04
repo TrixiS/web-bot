dd6777d4adb1575128eb0f2a667a33291299b495
TrixiS <oficialmorozov@gmail.com>
2021-08-04 22:19:07 +0500

    Remove start logic from bot main

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,26 +1,10 @@
 import os
 import re
 import argparse
-import logging
 
 from pathlib import Path
 
 from bot import root_path
-from . import config
-from .bot import Bot
-
-logging.basicConfig(
-    filename=None if config.debug else root_path / "logs.log",
-    level=logging.WARNING if config.debug else logging.ERROR,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s",
-)
-
-
-def get_all_extensions(cogs_path: Path):
-    ext_paths = (
-        p for p in cogs_path.glob("*.py") if p.is_file() and not p.name.startswith("_")
-    )
-    return [f"bot.cogs.{p.stem}" for p in ext_paths]
 
 
 def cog_path_from_name(cogs_path: Path, cog_name: str) -> Path:
@@ -69,15 +53,6 @@ def main():
 
         if args.jump:
             os.system(f"code {cog_path.absolute()}")
-
-        return
-
-    bot = Bot()
-
-    for ext in get_all_extensions(cogs_path):
-        bot.load_extension(ext)
-
-    bot.run()
 
 
 main()
